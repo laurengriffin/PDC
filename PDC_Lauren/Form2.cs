097e38f02f6dc7b478d02eeba7413da6284ef2ef
laurenarfaras <larfaras96@gmail.com>
2018-06-04 14:05:18 -0600

    can now determine primary keys on the sql server and does not modify them

--- PDC_Lauren/Form2.cs
+++ PDC_Lauren/Form2.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
@@ -45,28 +46,52 @@ namespace PDC_Lauren
             {
                 using (SqlConnection sqlc = new SqlConnection(SQLCommunication.cs))
                 {
+                    // open sql connection
                     sqlc.Open();
-                    // create the update command
-                    //SqlCommand updateCommand = new SqlCommand($"UPDATE {SQLCommunication.tableName} SET inventory=@Inventory where ingrCode=@IngrCode", sqlc);
-                    //updateCommand.Parameters.AddWithValue("@Inventory", 3);
-                    //updateCommand.Parameters.AddWithValue("@IngrCode", "RIC 30");
-                    //int rowsAffected = updateCommand.ExecuteNonQuery();
-                    //if (rowsAffected == 1)
-                    //{
-                    //    MessageBox.Show("Information Updated", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    //}
 
-
-                    string strQuery = string.Empty;
-                    //DataTable dtUpdated = (DataTable)dataGridView1.DataSource;
                     SqlCommand objCmd = new SqlCommand();
+
+                    // find primary keys
+                    string keyQuery = "SELECT c.Name AS 'Column_Name' " +
+                                        "FROM sys.indexes i " +
+                                        "INNER JOIN sys.index_columns ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id " +
+                                        "INNER JOIN sys.columns c ON ic.object_id = c.object_id AND ic.column_id = c.column_id " +
+                                        "INNER JOIN sys.objects o ON i.object_id = o.object_id " +
+                                        "WHERE i.is_primary_key = 1 " +
+                                        $"AND o.name = '{SQLCommunication.tableName}'";
+                    objCmd.CommandText = keyQuery;
+                    objCmd.CommandType = CommandType.Text;
+                    objCmd.Connection = sqlc;
+
+                    SqlDataReader reader = objCmd.ExecuteReader();
+
+                    // create string of keys
+                    int numKeys = 0;
+                    String keyString = "";
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader.GetString(0)}");
+                            keyString += reader.GetString(0) + ";";
+                            numKeys++;
+                        }
+                    }
+                    // string of keys
+                    string[] keys = Regex.Split(keyString, ";");
+                    for (int i = 0; i < keys.Length-1; i++)
+                    {
+                        Console.WriteLine($"keys[{i}]: {keys[i]}");
+                    }
+
+                    // initialize query string and sqlcommand object
+                    string strQuery = string.Empty;
 
                     // get the modified rows by filtering on their rowstate
                     DataTable dtChanges = gridTable.GetChanges(DataRowState.Modified);
                     if (dtChanges != null)
                     {
                         // for the update query to update the rows
-                        objCmd.Connection = sqlc;
                         for (int i = 0; i < dtChanges.Rows.Count; i++)
                         {
                             strQuery = $"UPDATE {SQLCommunication.tableName} SET ";
@@ -75,26 +100,35 @@ namespace PDC_Lauren
                             Console.WriteLine($"number of colums: {dtChanges.Columns.Count}");
                             for (int j = 0; j < dtChanges.Columns.Count; j++)
                             {
-                                if (j != dtChanges.Columns.Count - 1)
+
+                                int index = Array.IndexOf(keys, $"{dtChanges.Columns[j]}");
+                                Console.WriteLine($"index of {dtChanges.Columns[j]}: {index}");
+                                if (index < 0)
                                 {
-                                    strQuery += $"{dtChanges.Columns[j]} = '" + dtChanges.Rows[i][$"{dtChanges.Columns[j]}"].ToString() + "',";
+                                    if (j != dtChanges.Columns.Count - 1)
+                                    {
+                                        strQuery += $"{dtChanges.Columns[j]} = '" + dtChanges.Rows[i][$"{dtChanges.Columns[j]}"].ToString() + "',";
+                                    }
+                                    else
+                                    {
+                                        strQuery += $"{dtChanges.Columns[j]} = '" + dtChanges.Rows[i][$"{dtChanges.Columns[j]}"].ToString() + "' ";
+                                    }
                                 }
-                                else
-                                {
-                                    strQuery += $"{dtChanges.Columns[j]} = '" + dtChanges.Rows[i][$"{dtChanges.Columns[j]}"].ToString() + "' "; 
-                                }
-                                //Console.WriteLine($"col[{j}]: {dtChanges.Columns[j]}");
                             }
-                            //strQuery += $"WHERE {gridTable.Columns[0]} = '" + gridTable.Columns[0].ToString() + "'";
-                            strQuery += $"WHERE {gridTable.Columns[0]} = '" + gridTable.Rows[i][$"{gridTable.Columns[0]}"].ToString() + "'";
+                            strQuery += $"WHERE ";
+                            for (int k = 0; k < keys.Length-1; k++)
+                            {
+                                strQuery += $"{gridTable.Columns[keys[k]]} = '" + gridTable.Rows[i][$"{gridTable.Columns[keys[k]]}"].ToString() + "'";
+                                if (k != keys.Length-2)
+                                {
+                                    strQuery += " AND ";
+                                }
+                            }
+                            //strQuery += $"WHERE {gridTable.Columns[0]} = '" + gridTable.Rows[i][$"{gridTable.Columns[0]}"].ToString() + "'";
                         }
 
-                        // find the key
-                        //Console.WriteLine($"number of keys: {}");
-                        //for (int i = 0; i < gridTable.PrimaryKey.length; i++)
-                        //{
-                            //Console.WriteLine($"The Key: {gridTable.PrimaryKey[i]}");
-                        //}
+                        reader.Close();
+
                         Console.WriteLine($"Query String:\n{strQuery}");
                         objCmd.CommandText = strQuery;
                         objCmd.ExecuteNonQuery();
@@ -102,7 +136,6 @@ namespace PDC_Lauren
                     }
                     sqlc.Close();
                     dtChanges = null;
-                    //gridAdapter.UpdateCommand = updateCommand;
                 }
             }
             catch (Exception ex)
